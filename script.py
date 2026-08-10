a9d2506da60f25f99f178ab9087ef943ade1ccad
desperado999999 <yjq20080000@hotmail.com>
2026-08-10 19:26:58 +0800

    判断奇偶

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
-print("Hello World")
-name=input("Enter your name")
-print(f'你好，{name}！欢迎开始编程之旅')
+number=input("请输入一个整数：")
+num=int(number)
+if num%2==0:
+    print(f"{num}是偶")
+else:
+    print(f'{num}是奇')
+print("结束，欢迎下次使用")
