17fa3cb2ce7f3a9f47ebc4447c8347c0cd7b5d7f
desperado999999 <yjq20080000@hotmail.com>
2026-08-11 09:43:32 +0800

    零基索引与指针

--- script.py
+++ script.py
@@ -1,21 +1,9 @@
-numbers=[]
+num=[12,3,5,6,8]
 total=0
-print("计算器")
-print("输入数字，输入结束，得总和与平均")
-while True:
-    userinput=input(">>>输入数字或输入‘结束’退出")
-    if userinput=="结束":
-        break
-    num=int(userinput)
-    numbers.append(num)
-    total=total+num
-    print(f"已输入{num},共{len(numbers)}")
-print("\n"+"="*20)
-print(f"共{len(numbers)}")
-for num in numbers:
-    if num%2==0:
-        print(f"{num}是偶")
-    else:
-        print(f"{num}是奇")
-print(f"总和{total}")
-print(f"平均数是：{total/len(numbers)}")
+index=0
+while index<len(num):
+    n=num[index]
+    total=total+n
+    print(f"加了第{index+1}个数：{n},当前总和{total}")
+    index=index+1
+print(f"最终总和{total}")
