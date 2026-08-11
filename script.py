d98a3b1d85a2f67dd311760d1ad96c8f6d655b0e
desperado999999 <yjq20080000@hotmail.com>
2026-08-11 12:28:10 +0800

    引入def,实现自动分析，交互输入

--- script.py
+++ script.py
@@ -1,24 +1,34 @@
-numbers=[]
-total=0
-print("计算器")
-print("输入数字，输入结束，得总和与平均")
-while True:
-    userinput=input(">>>输入数字或输入‘结束’退出")
-    if userinput=="结束":
-        break
-    if not userinput.isdigit():
-        print("警告，请输入纯数字")
-        continue
-    num=int(userinput)
-    numbers.append(num)
-    total=total+num
-    print(f"已输入{num},共{len(numbers)}")
-print("\n"+"="*20)
-print(f"共{len(numbers)}")
-for num in numbers:
-    if num%2==0:
-        print(f"{num}是偶")
-    else:
-        print(f"{num}是奇")
-print(f"总和{total}")
-print(f"平均数是：{total/len(numbers)}")
+def 自动分析(数字列表):
+    """给数字，立刻算"""
+    print("\n---自动分析报告")
+    total=0
+    for n in 数字列表:
+        if n%2==0:
+            print(f"{n}是偶")
+        else:
+            print(f"{n}是奇")
+        total=total+n
+    print(f"总和是{total}")
+    print(f"平均数是{total/len(数字列表)}")
+    print("---报告结束---")
+def 交互输入():
+    """输入，边输边分析"""
+    numbers=[]
+    total=0
+    print("输入数字，输入‘结束’退出")
+    while True:
+        userinput=input()
+        if userinput=="结束":
+            break
+        if not userinput.isdigit():
+            print("警告")
+            continue
+        num=int(userinput)
+        numbers.append(num)
+        total=total+num
+        print(f"已收录{num},共{len(numbers)}个数")
+    自动分析(numbers)
+print("欢迎来到数据分析工具箱")
+自动分析([10,28,90])
+交互输入()
+print("感谢使用，下次再见")
