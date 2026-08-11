df3c4d3addfa3e3507f0fd246122037ad70209c6
desperado999999 <yjq20080000@hotmail.com>
2026-08-11 16:25:34 +0800

    return

--- script.py
+++ script.py
@@ -1,34 +1,27 @@
-def 自动分析(数字列表):
-    """给数字，立刻算"""
-    print("\n---自动分析报告")
+def 计算统计(数字列表):
     total=0
+    for n in 数字列表:
+        total=total+n
+    平均数=total/len(数字列表)
+    return total,平均数
+def 显示报告(数字列表):
+    总和,均值=计算统计(数字列表)
+    print("\n---奇偶分析---")
     for n in 数字列表:
         if n%2==0:
             print(f"{n}是偶")
         else:
             print(f"{n}是奇")
-        total=total+n
-    print(f"总和是{total}")
-    print(f"平均数是{total/len(数字列表)}")
-    print("---报告结束---")
-def 交互输入():
-    """输入，边输边分析"""
-    numbers=[]
-    total=0
-    print("输入数字，输入‘结束’退出")
-    while True:
-        userinput=input()
-        if userinput=="结束":
-            break
-        if not userinput.isdigit():
-            print("警告")
-            continue
-        num=int(userinput)
-        numbers.append(num)
-        total=total+num
-        print(f"已收录{num},共{len(numbers)}个数")
-    自动分析(numbers)
-print("欢迎来到数据分析工具箱")
-自动分析([10,28,90])
-交互输入()
-print("感谢使用，下次再见")
+    print(f"总和是{总和}")
+    print(f"均值是{均值}")
+    print("___报告结束--\n")
+数据=[14,27,39]
+计算结果=计算统计(数据)
+print(f"计算器返回的原始包裹：{计算结果}")
+总和,平均数=计算统计(数据)
+if 平均数>10:
+     print(f"平均数{平均数}大于10！")
+else:
+     print(f"平均数{平均数}正常")
+显示报告(数据)
+
